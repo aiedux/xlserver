0318643b52f8b2d996a6b8831c606457abad2eac
stefian <stefan.ianta@w3ai.net>
2020-02-16 17:39:48 -0400

    added a default call record in Redis

--- app.js
+++ app.js
@@ -49,6 +49,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+client.hmset('call', ['name', 'social memoization', 'company', 'CMD.co', 'phone', '365-247-2020', 'time', 'noon']);
+
 app.get('/', function (req, res) {
   let title = 'social memory';
 
